52206727cf030d9f76517a6e209d47ee3c5271e3
Jon Malis <jmalis@live.com>
2016-01-18 10:19:46 -0700

    services1 checkpoint

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,9 @@
 (function() {
-     function AlbumCtrl() {
-			 this.albumData = angular.copy(albumPicasso);
+     function AlbumCtrl(Fixtures) {
+			 this.albumData = Fixtures.getAlbum();
      }
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', AlbumCtrl);
+         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
  })();
