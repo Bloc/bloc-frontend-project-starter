a8991798eb8f627800eb1e7c30fdef053b53d4b8
Jon Malis <jmalis@live.com>
2016-01-20 14:17:52 -0700

    services3 checkpoint

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,13 +1,15 @@
 (function() {
-	function SongPlayer() {
+	function SongPlayer(Fixtures) {
 		var SongPlayer = {};
-	
 		
- /**
- * @desc Currently selected song
- * @type {Object} 
- */
-		var currentSong = null;
+		
+	/**
+	* @desc Stores album info in currentAlbum variable
+	* @type {Object} 
+	*/
+		
+		var currentAlbum = Fixtures.getAlbum();
+	
 		
  /**
  * @desc Buzz object audio file
@@ -25,7 +27,7 @@
 		var setSong = function(song) {
 				if (currentBuzzObject) {
 						currentBuzzObject.stop();
-						currentSong.playing = null;
+						SongPlayer.currentSong.playing = null;
 				} 
 			
 			currentBuzzObject = new buzz.sound(song.audioURL, {
@@ -33,7 +35,7 @@
 				preload: true
 			});
 				
-				currentSong = song;
+				SongPlayer.currentSong = song;
 		};
 		
 	/**
@@ -47,12 +49,29 @@
 			song.playing = true;
 		};
 		
+	/**
+	* @function getSongIndex
+	* @desc Stores the song index in the getSongIndex variable
+	* @param {Object} song
+	*/
+		
+		var getSongIndex = function(song) {
+			return currentAlbum.songs.indexOf(song);
+		};
+		
+	/**
+ 	* @desc Initializes currently selected song to null
+ 	* @type {Object} 
+ 	*/
+		SongPlayer.currentSong = null;
+		
 		
 		SongPlayer.play = function(song) {
-			if (currentSong !== song) {
+			song = song || SongPlayer.currentSong;
+			if (SongPlayer.currentSong !== song) {
 				setSong(song);
 				playSong(song);
-			} else if (currentSong === song) {
+			} else if (SongPlayer.currentSong === song) {
 						if (currentBuzzObject.isPaused()) {
 								currentBuzzObject.play();
 								song.playing = true;
@@ -61,9 +80,36 @@
 		};
 		
 		SongPlayer.pause = function(song) {
+			song = song || SongPlayer.currentSong;
 			currentBuzzObject.pause();
 			song.playing = false
 			setSong(song);
+		};
+		
+	/**
+	*	@desc Stores currently playing song index in the currentSongIndex variable
+	* @type {Object} 
+	*/
+		
+		
+		
+	/**
+	* @function SongPlayer.previous
+	* @desc Decreaces the current song index by one, and plays the previous song. Unless the index is already zero, then the 				 player will remain on the first song
+	*/
+		
+		SongPlayer.previous = function() {
+			var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+			currentSongIndex--;
+			
+			if (currentSongIndex < 0) {
+				currentBuzzObject.stop();
+				SongPlayer.currentSong.playing = null;
+			} else {
+				var song = currentAlbum.songs[currentSongIndex];
+				setSong(song);
+				playSong(song);
+			}
 		};
 		
 		return SongPlayer;
@@ -71,5 +117,5 @@
 	
 	angular
 		.module('blocJams')
-		.service('SongPlayer', SongPlayer);
+		.service('SongPlayer', ['Fixtures', SongPlayer]);
 })();
