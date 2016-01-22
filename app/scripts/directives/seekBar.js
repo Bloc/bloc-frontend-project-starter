c100d1eabdb947468fb13a528fcd3a77a64471ab
Jon Malis <jmalis@live.com>
2016-01-22 11:12:22 -0700

    directives1 assignment

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -33,6 +33,10 @@
 					return {width: percentString()};
 				};
 				
+				scope.thumbStyle = function() {
+					return {left: percentString()};
+				};
+				
 				scope.onClickSeekBar = function(event) {
 					var percent = calculatePercent(seekBar, event);
 					scope.value = percent * scope.max;
