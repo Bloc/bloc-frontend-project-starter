c916888a1d516475af21e8c2f35e822352f9117f
Brian Douglas <bdougie@users.noreply.github.com>
2016-11-11 18:13:56 -0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,7 +74,6 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-hapi');
 
